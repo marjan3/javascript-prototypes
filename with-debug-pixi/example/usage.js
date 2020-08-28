c91d843f2dda2d7824da4f355eac5fcfc2bb923e
Marjan Tanevski <marjan.tanevski@outlook.com>
2020-08-28 11:52:45 +0200

    removed pixi projects

--- with-debug-pixi/example/usage.js
+++
@@ -1,7 +0,0 @@
-import Observable from "../src/debug-pixi";
-
-const o = new Observable();
-o.on("test", (params)=> {
-    alert("test called with params: " + JSON.stringify(params));
-});
-o.emit("test", 1, "asdasd");
